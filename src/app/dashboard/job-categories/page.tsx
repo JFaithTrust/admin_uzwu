6d4d14bcec4b935bb9c184a5bc4117adc3669113
Abdurasul <abdurasulabduraimov22@gmail.com>
2024-08-09 12:06:34 +0500

    job category page improved

--- src/app/dashboard/job-categories/page.tsx
+++ src/app/dashboard/job-categories/page.tsx
@@ -22,11 +22,9 @@ import Link from "next/link";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import useJobCategoryStore from "@/store/job-category-store";
 import {TableSkeleton} from "@/components/skeletons/table-skeleton";
+import { DataTable } from "@/components/layout/data-table";
 
 const JobTypes = () => {
-  const [sorting, setSorting] = useState<SortingState>([])
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
 
   const { jobCategories, getJobCategories } = useJobCategoryStore()
 
@@ -34,145 +32,10 @@ const JobTypes = () => {
     getJobCategories().then()
   }, [getJobCategories]);
 
-  const table = useReactTable({
-    data: jobCategories,
-    columns: jobCategoryColumns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility
-    }
-  })
 
   return (
     <div className={"w-full"}>
-      <div className={"flex items-center justify-between py-4"}>
-        <Input
-          placeholder="Filter by titles..."
-          value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("title")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
-        <div className={"flex gap-x-4"}>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="ml-auto">
-                Columns <ChevronDown className="ml-2 h-4 w-4"/>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align={"end"}>
-              {
-                table.getAllColumns()
-                  .filter((column) => column.getCanHide())
-                  .map((column) => {
-                    return (
-                      <DropdownMenuCheckboxItem
-                        key={column.id}
-                        className="capitalize"
-                        checked={column.getIsVisible()}
-                        onCheckedChange={(value) =>
-                          column.toggleVisibility(!!value)
-                        }
-                      >
-                        {column.id}
-                      </DropdownMenuCheckboxItem>
-                    )
-                  })
-              }
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <Link href={"/dashboard/job-categories/create-job-category"}>
-            <Button
-              variant="outline"
-              className="ml-auto bg-sky-100 text-blue-600 hover:text-blue-600/85 border-blue-500"
-            >
-              Create
-            </Button>
-          </Link>
-        </div>
-      </div>
-      <div className={"rounded-md border"}>
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={jobCategoryColumns.length}
-                  className="h-24 text-center"
-                >
-                  <TableSkeleton/>
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        {/*<div className="flex-1 text-sm text-muted-foreground">*/}
-        {/*  {table.getFilteredSelectedRowModel().rows.length} of{" "}*/}
-        {/*  {table.getFilteredRowModel().rows.length} row(s) selected.*/}
-        {/*</div>*/}
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
-      </div>
+      <DataTable columns={jobCategoryColumns} data={jobCategories} updateData={getJobCategories} rowCount={jobCategories.length} />
     </div>
   );
 };
